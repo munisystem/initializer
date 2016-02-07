f9aeea36d858483b8e4a0bcfad6bc68727e8b539
munisystem <munisystem@gmail.com>
2016-02-07 20:18:05 +0900

    Update rbenv cookbook

--- cookbooks/rbenv/default.rb
+++ cookbooks/rbenv/default.rb
@@ -3,4 +3,11 @@ execute 'Install ruby' do
     brew link readline --force
     MAKE_OPTS="-j 4" RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" rbenv install 2.3.0
   EOL
+  not_if 'rbenv versions | grep 2.3.0'
+  notifies :run, 'execute[Set global ruby]', :immediately
+end
+
+execute 'Set global ruby' do
+  command 'rbenv global 2.3.0'
+  action :nothing
 end
