f907f1eabe64b505ad9c9dc9dd04c7ffb1297b93
munisystem <munisystem@gmail.com>
2016-03-20 19:26:42 +0900

    Update

--- cookbooks/dotfiles/default.rb
+++ cookbooks/dotfiles/default.rb
@@ -1,7 +1,14 @@
-execute 'Download my dotfiles' do
-  command 'curl -sL https://raw.githubusercontent.com/munisystem/dotfiles/master/install.sh | sh'
+# execute 'Download my dotfiles' do
+#   command 'curl -sL dots.munisystem.net | sh'
+# end
+
+template '/etc/shells' do
+  source './templates/shells'
+  owner 'root'
+  group 'wheel'
+  mode '0644'
 end
 
 execute 'Set default shell zsh' do
-  command 'chsh -s /bin/zsh'
+  command 'chsh -s /usr/local/bin/fish'
 end
